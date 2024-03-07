55de5a9b7a079f565b5be5665fd423b07ca03e20
Nikhil Sharma <nikhil@birdseye.ca>
2024-03-07 09:51:39 -0800

    First commit : View Readme for details

---
+++ files.bat
@@ -0,0 +1,24 @@
+cd ./Documents/multiTarget_multiCamera_tracking
+dir /b > files.txt
+cd..
+cd..
+cd ./Documents/opticalFlow
+dir /b > files.txt
+cd..
+cd..
+cd ./Documents/pedestrian_tracking
+dir /b > files.txt
+cd..
+cd..
+cd ./Documents/segmentation
+dir /b > files.txt
+cd..
+cd..
+cd ./Documents/singleCamera_tracking
+dir /b > files.txt
+cd..
+cd..
+cd ./Documents/classicalTracking
+dir /b > files.txt
+cd..
+cd..
